527d7ebcf158eb7511dc167c930166328c62ec9a
DagmawiZelelew <dagipeacu@gmail.com>
2026-08-18 20:58:45 +0300

    Implement Library system with classes
    Added LibraryItem, Book, EBook, and Library classes.

--- Javascript/Practice4.js
+++ Javascript/Practice4.js
@@ -1,14 +1,58 @@
-class contact{
-    constructor(name,phone_no){
-        this.name =name;
-        this.phone_no =phone_no;
+// ==========================================
+// TASK 1: CREATE A BASE LIBRARY ITEM CLASS
+// ==========================================
 
-    }
-    static displaycontact(){
-        console.log("Contact successfully saved");
+// This will act as the parent class
+class LibraryItem {
+    constructor(title, author, pub_year, avail_status = true) {
+        this.title = title;
+        this.author = author;
+        this.pub_year = pub_year;
+        this.avail_status = avail_status;
     }
 }
-const contact1 = new contact("Yoseph",947899506);
-const contact2 = new contact("Dagmawi",923231226);
-console.log(contact1.name);
-contact.displaycontact();
+
+
+// ==========================================
+// TASK 2: CREATE THE BOOK CLASS
+// ==========================================
+
+// Book inherits properties from LibraryItem
+class Book extends LibraryItem {
+
+    constructor(title, author, pub_year, avail_status = true) {
+
+        // Call the parent constructor
+        super(title, author, pub_year, avail_status);
+    }
+}
+// ==========================================
+// TASK 3: CREATE THE EBOOK CLASS
+// ==========================================
+
+// EBook inherits from Book
+class EBook extends Book {
+
+    constructor(title, author, pub_year, avail_status, fileFormat) {
+
+        // Call the Book constructor
+        super(title, author, pub_year, avail_status);
+
+        // Additional property for an EBook
+        this.fileFormat = fileFormat;
+    }
+}
+
+
+// ==========================================
+// TASK 4: CREATE THE LIBRARY CLASS
+// ==========================================
+
+class Library {
+
+    constructor() {
+
+        // An array to store all books
+        this.books = [];
+    }
+
