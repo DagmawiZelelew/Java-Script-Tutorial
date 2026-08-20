c39a9571da1f8182c4463bf726c9e3f6cfbba2f3
Dagmawi Zelelew <Dagmawizelelew2007@gmail.com>
2026-08-20 06:32:40 +0300

    Built a World Clock System

--- Javascript/Practice5.js
+++ Javascript/Practice5.js
@@ -1,12 +1,13 @@
-let timeoutID;
-function timer(){
-timeoutID = setTimeout(function(){
-    window.alert("HEYYY BUDDYYY!");
-    console.log("started")
-},3000)}
+let time = document.getElementById("clock")
+function updateClock(){
+    const curr = new Date();
+    const hours = curr.getHours().toString().padStart(2,0);
+    const minutes = curr.getMinutes().toString().padStart(2,0);
+    const seconds = curr.getSeconds().toString().padStart(2,0); 
+    time.textContent = `${hours} : ${minutes} : ${seconds}`
+     
 
-function stop(){
-    clearTimeout(timeoutID)
-    console.log("Cancelled")
 }
+updateClock();
+setInterval(updateClock,1000);
 
